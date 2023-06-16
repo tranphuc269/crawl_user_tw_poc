a8dc7548225ce3a730d4e07fa43d83ce52dde69c
Trần Văn Phúc <phuc260900@gmail.com>
2023-06-16 12:22:20 +0700

    update crawl using selenium

--- main.py
+++ main.py
@@ -1,36 +1,162 @@
-import subprocess
-import time
-from concurrent.futures import ThreadPoolExecutor, as_completed
-
-
-curl_command = '''
-curl --location 'https://twitter.com/i/api/1.1/search/typeahead.json?include_ext_is_blue_verified=1&include_ext_verified_type=1&include_ext_profile_image_shape=1&q=phuc&src=search_box&result_type=events%2Cusers%2Ctopics' \
---header 'Authorization: Bearer AAAAAAAAAAAAAAAAAAAAANRILgAAAAAAnNwIzUejRCOuH5E6I8xnZz4puTs%3D1Zv7ttfk8LF81IUq16cHjhLTvJu4FA33AGWWjCpTnA' \
---header 'Cookie: guest_id=v1%3A167176499478279878; _ga=GA1.2.1579385385.1672718365; guest_id_marketing=v1%3A167176499478279878; guest_id_ads=v1%3A167176499478279878; g_state={"i_l":0}; kdt=enc1yMa6OiY8wzDb6eBhXUBG5gQdV4gPU5Pirhrn; auth_token=9ba194ed89e09f8d29117584ffc4335e5f7536f0; ct0=eb9f4fc6f9e062bb6b30d5d81cd3764a568823b099882d25293e1dc643f53dec582bdbe99a076b9040752f6bf04794ad0c6184993a3b84d381b5689ee932b41c19c752f66eb9998b60816964b8f13b81; twid=u%3D1456283845561487361; lang=en; _gid=GA1.2.1463282300.1686732168; personalization_id="v1_ZZv4YjTad2TkcqtPk3AeKA=="; ct0=3b86c864150828855b3f5b983103a3bf366edafd9d70bd5e21fa866fca8aa85c30414114fee631e2ad3cce6cc24669326959cfcde036a7663258695487656d38f29e70b2c5a9f1ff3c161bfa0686e0e6; guest_id=v1%3A168673323957335130; guest_id_ads=v1%3A168673323957335130; guest_id_marketing=v1%3A168673323957335130; personalization_id="v1_y8+HmZ5a2vLX5tYLbwJzhw=="; ct0=3b86c864150828855b3f5b983103a3bf366edafd9d70bd5e21fa866fca8aa85c30414114fee631e2ad3cce6cc24669326959cfcde036a7663258695487656d38f29e70b2c5a9f1ff3c161bfa0686e0e6; guest_id=v1%3A168673323957335130; guest_id_ads=v1%3A168673323957335130; guest_id_marketing=v1%3A168673323957335130; personalization_id="v1_y8+HmZ5a2vLX5tYLbwJzhw=="' \
---header 'X-Csrf-Token: eb9f4fc6f9e062bb6b30d5d81cd3764a568823b099882d25293e1dc643f53dec582bdbe99a076b9040752f6bf04794ad0c6184993a3b84d381b5689ee932b41c19c752f66eb9998b60816964b8f13b81'
 '''
-duration = 10  # second
-num_threads = 4  #
+Description: Crawl data twitter using selenium
 
-def execute_curl():
-    process = subprocess.Popen(curl_command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-    output, error = process.communicate()
+Authors: Tran Van Phuc,
 
-    if process.returncode == 0:
-        return "Success"
-    elif process.returncode == 403:
-        return "API blocked"
-    else:
-        return "Something wrong!"
+Date: Fri 15, 2023
 
-start_time = time.time()
+'''
 
-with ThreadPoolExecutor(max_workers=num_threads) as executor:
-    futures = []
-    while (time.time() - start_time) < duration:
-        futures.append(executor.submit(execute_curl))
-        time.sleep(1)
+import csv
+from getpass import getpass
+from time import sleep
+from selenium.webdriver.common.keys import Keys
+from selenium.common.exceptions import NoSuchElementException
+from selenium.webdriver import Chrome
 
-    for future in as_completed(futures):
-        result = future.result()
-        print(result)
+
+def parseData(post):
+    # get userName
+    name = post.find_element("xpath", './/span').text
+
+    # get date
+    try:
+        date = post.find_element("xpath", './/time').get_attribute('datetime')
+    except NoSuchElementException:
+        return
+
+    # get content
+    content = post.find_element("xpath", './/div[1]/div[1]/div[2]/div[2]/div[2]').text
+    # check if the tweet is replying to someone.
+    # if it does, move to .../div[3] to read the content of the tweet
+    if content.startswith('Replying to'):
+        content = post.find_element("xpath", './/div[1]/div[1]/div[2]/div[2]/div[3]').text
+
+    # get reply count
+    replyCnt = post.find_element("xpath", './/div[@data-testid="reply"]').text
+    if replyCnt == '':
+        # set none to default value 0
+        replyCnt = '0'
+
+    # get retweet count
+    retweetCnt = post.find_element("xpath", './/div[@data-testid="retweet"]').text
+    if retweetCnt == '':
+        # set none to default value 0
+        retweetCnt = '0'
+
+    # get like count
+    likeCnt = post.find_element("xpath", './/div[@data-testid="like"]').text
+    if likeCnt == '':
+        # set none to default value 0
+        likeCnt = '0'
+
+    # create a tuple and fill its values
+    tweet = (name, date, content, replyCnt, retweetCnt, likeCnt)
+    return tweet
+
+
+# ask for username, email, topic, password, and number of tweets
+# userName = input('Enter your username: ')
+# email = input('Enter your email address: ')
+# topic = input('Enter your topic: ')
+# password = getpass('Enter your password: ')
+userName = 'tranphuc269'
+email = 'phuc260900@gmail.com'
+topic = 'game'
+password = 'jerrytran97'
+valid = False
+while not (valid):
+    num = input('Approximately how many tweets you want to collect: ')
+    if num.isdigit():
+        valid = True
+
+# use Chrome as the default browser
+driver = Chrome()
+
+# open twitter
+driver.get('https://twitter.com/i/flow/login')
+driver.maximize_window()
+sleep(35)
+
+# enter email and next
+emailAddr = driver.find_element("xpath", '//input[@name="text"]')
+emailAddr.send_keys(email)
+emailAddr.send_keys(Keys.RETURN)
+sleep(10)
+
+# enter name and next
+try:
+    name = driver.find_element("xpath", '//input[@name="text"]')
+    name.send_keys(userName)
+    name.send_keys(Keys.RETURN)
+    sleep(10)
+# check if we need to enter username this time
+except NoSuchElementException:
+    print("Don't need to enter username this time. Keep waiting.")
+
+# enter password and next
+passwords = driver.find_element("xpath", '//input[@name="password"]')
+passwords.send_keys(password)
+passwords.send_keys(Keys.RETURN)
+# sleep(15)
+
+# search the topic
+sleep(40)
+searchLabel = driver.find_element("xpath", '//input[@aria-label="Search query"]')
+searchLabel.send_keys(topic)
+searchLabel.send_keys(Keys.RETURN)
+sleep(30)
+driver.find_element("link text", 'Latest').click()
+sleep(30)
+
+data = []
+tweet_ids = set()
+last_position = driver.execute_script("return window.pageYOffset;")
+scrolling = True
+enough = False
+
+while scrolling:
+    try:
+        posts = driver.find_elements("xpath",
+                                     '//article[@data-testid="tweet"]')  # read all tweets on the current page and store
+        # them as a list
+        for post in posts[-15:]:
+            tweet = parseData(post)  # parse the tweets
+            if tweet:
+                tweet_id = ''.join(tweet)
+                if tweet_id not in tweet_ids:
+                    tweet_ids.add(tweet_id)
+                    data.append(tweet)
+                    if (len(data) >= int(num)):  # check if enough data has been collected
+                        enough = True
+                        break
+
+        if enough == True:
+            break
+        scroll_attempt = 0
+        while True:
+            driver.execute_script('window.scrollTo(0, document.body.scrollHeight);')
+            sleep(10)
+            curr_position = driver.execute_script("return window.pageYOffset;")
+            if last_position == curr_position:
+                scroll_attempt += 1
+
+                # end of scroll region
+                if scroll_attempt >= 3:
+                    scrolling = False
+                    break
+                else:
+                    sleep(10)  # attempt another scroll
+            else:
+                last_position = curr_position
+                break
+    except:
+        break
+driver.close()
+
+# create a csv file and write data into the file
+with open('tweets.csv', 'w', newline='', encoding='utf-8') as f:
+    header = ['Username', 'Time', 'Content', 'Reply Count', 'Retweet Count', 'Like Count']
+    writer = csv.writer(f)
+    writer.writerow(header)
+    writer.writerows(data)
