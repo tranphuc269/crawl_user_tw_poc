053285e4a5a51fa611e02f363d877c73e3655350
Trần Văn Phúc <90142169+tranphuc269@users.noreply.github.com>
2023-07-24 17:50:44 +0700

    Update test.py

--- test.py
+++ test.py
@@ -4,8 +4,7 @@ from Crypto.Random import get_random_bytes
 from Crypto.Protocol.KDF import PBKDF2
 from Crypto.Util.Padding import pad
 from minio import Minio
-import tempfile
-import os
+import io
 
 app = FastAPI()
 
@@ -31,14 +30,14 @@ async def upload_file(file: UploadFile = File(...), password: str = None):
     # Initialize the AES cipher in CBC mode with the derived key and a random IV
     cipher = AES.new(key, AES.MODE_CBC)
 
-    # Create a temporary file to store the encrypted data
-    temp_file = tempfile.NamedTemporaryFile(delete=False)
+    # Create an in-memory buffer to store the encrypted data
+    encrypted_data = io.BytesIO()
 
-    # Write the salt and IV to the temporary file
-    temp_file.write(salt)
-    temp_file.write(cipher.iv)
+    # Write the salt and IV to the buffer
+    encrypted_data.write(salt)
+    encrypted_data.write(cipher.iv)
 
-    # Encrypt the file and write it to the temporary file
+    # Encrypt the file and write it to the buffer
     while True:
         chunk = await file.read(8192)
         if not chunk:
@@ -46,59 +45,57 @@ async def upload_file(file: UploadFile = File(...), password: str = None):
         elif len(chunk) % AES.block_size != 0:
             # Pad the chunk to a multiple of the AES block size
             chunk = pad(chunk, AES.block_size)
-        temp_file.write(cipher.encrypt(chunk))
+        encrypted_data.write(cipher.encrypt(chunk))
 
-    # Close the temporary file
-    temp_file.close()
-
-    # Upload the encrypted file to MinIO
+    # Upload the encrypted data to MinIO
     minio_client = Minio(MINIO_ENDPOINT, access_key=MINIO_ACCESS_KEY, secret_key=MINIO_SECRET_KEY, secure=False)
-    with open(temp_file.name, 'rb') as file_data:
-        minio_client.put_object(MINIO_BUCKET_NAME, file.filename, file_data)
+    encrypted_data.seek(0)
+    minio_client.put_object(MINIO_BUCKET_NAME, file.filename, encrypted_data, len(encrypted_data.getvalue()))
 
-    # Delete the temporary file
-    os.remove(temp_file.name)
+    # Close the buffer
+    encrypted_data.close()
 
     return {"message": "File uploaded successfully."}
+from fastapi import FastAPI
+from Crypto.Cipher import AES
+from Crypto.Protocol.KDF import PBKDF2
+from Crypto.Util.Padding import unpad
+from minio import Minio
+
+app = FastAPI()
+
+# MinIO configuration
+MINIO_ENDPOINT = 'your_minio_endpoint'
+MINIO_ACCESS_KEY = 'your_minio_access_key'
+MINIO_SECRET_KEY = 'your_minio_secret_key'
+MINIO_BUCKET_NAME = 'your_minio_bucket_name'
+
+# AES encryption parameters
+AES_KEY_SIZE = 32  # AES-256
+SALT_SIZE = 16
+ITERATIONS = 100000
+
 @app.get("/download/{file_name}")
 async def download_file(file_name: str, password: str = None):
     # Download the encrypted file from MinIO
     minio_client = Minio(MINIO_ENDPOINT, access_key=MINIO_ACCESS_KEY, secret_key=MINIO_SECRET_KEY, secure=False)
-    temp_file = tempfile.NamedTemporaryFile(delete=False)
-    with open(temp_file.name, 'wb') as file_data:
-        file_data.write(minio_client.get_object(MINIO_BUCKET_NAME, file_name).read())
+    encrypted_data = minio_client.get_object(MINIO_BUCKET_NAME, file_name).read()
 
-    # Create a temporary file to store the decrypted data
-    decrypted_temp_file = tempfile.NamedTemporaryFile(delete=False)
+    # Read the salt and IV from the encrypted data
+    salt = encrypted_data[:SALT_SIZE]
+    iv = encrypted_data[SALT_SIZE:SALT_SIZE + AES.block_size]
 
-    # Open the input file (encrypted data)
-    with open(temp_file.name, 'rb') as file_in:
-        # Read the salt and IV from the input file
-        salt = file_in.read(SALT_SIZE)
-        iv = file_in.read(AES.block_size)
+    # Derive the key from the password and salt using PBKDF2
+    key = PBKDF2(password, salt, dkLen=AES_KEY_SIZE, count=ITERATIONS)
 
-        # Derive the key from the password and salt using PBKDF2
-        key = PBKDF2(password, salt, dkLen=AES_KEY_SIZE, count=ITERATIONS)
+    # Initialize the AES cipher in CBC mode with the derived key and IV
+    cipher = AES.new(key, AES.MODE_CBC, iv=iv)
 
-        # Initialize the AES cipher in CBC mode with the derived key and IV
-        cipher = AES.new(key, AES.MODE_CBC, iv=iv)
+    # Decrypt the data
+    decrypted_data = cipher.decrypt(encrypted_data[SALT_SIZE + AES.block_size:])
 
-        # Decrypt the file and write it to the temporary file
-        while True:
-            chunk = file_in.read(8192)
-            if not chunk:
-                break
-            decrypted_chunk = cipher.decrypt(chunk)
-            if len(decrypted_chunk) < len(chunk):
-                # Unpad the decrypted chunk if necessary
-                decrypted_chunk = unpad(decrypted_chunk, AES.block_size)
-            decrypted_temp_file.write(decrypted_chunk)
+    # Unpad the decrypted data
+    decrypted_data = unpad(decrypted_data, AES.block_size)
 
-    # Close the temporary files
-    temp_file.close()
-    decrypted_temp_file.close()
-
-    # Delete the encrypted temporary file
-    os.remove(temp_file.name)
-
-    return FileResponse(decrypted_temp_file.name, media_type='application/octet-stream', headers={'Content-Disposition': f'attachment; filename="{file_name}"'})
+    # Return the decrypted data as a file response
+    return StreamingResponse(io.BytesIO(decrypted_data), media_type='application/octet-stream', headers={'Content-Disposition': f'attachment; filename="{file_name}"'})
