57558c8c36f3c2942a3cbc9d3dacd57345121727
Trần Văn Phúc <90142169+tranphuc269@users.noreply.github.com>
2023-07-25 11:34:37 +0700

    Update test.py

--- test.py
+++ test.py
@@ -1,5 +1,28 @@
-from fastapi import UploadFile
-import io
+import base64
 
-def bytesio_to_uploadfile(bytes_io: io.BytesIO, filename: str, content_type: str) -> UploadFile:
-    return UploadFile(filename=filename, content_type=content_type, file=bytes_io)
+def base64_decode_with_padding(base64_string):
+    # Kiểm tra xem chuỗi base64 có độ dài chia hết cho 4 hay không
+    padding_needed = len(base64_string) % 4
+    if padding_needed > 0:
+        base64_string += "=" * (4 - padding_needed)
+
+    # Giải mã chuỗi base64 thành dữ liệu ban đầu
+    try:
+        decoded_data = base64.b64decode(base64_string)
+        return decoded_data
+    except Exception as e:
+        print("Error decoding base64:", e)
+        return None
+
+# Chuỗi base64 cần giải mã
+base64_string = "SGVsbG8gV29ybGQh"
+
+# Giải mã base64 với xử lý padding
+decoded_data = base64_decode_with_padding(base64_string)
+
+# Chuyển đổi dữ liệu giải mã thành chuỗi (nếu là dữ liệu văn bản)
+if decoded_data:
+    decoded_text = decoded_data.decode('utf-8')
+    print(decoded_text)  # Kết quả: "Hello World!"
+else:
+    print("Invalid base64 string.")
